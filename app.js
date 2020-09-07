a54b240e20c6fa899c9a5d1d79e5115b46ee8e6a
Mariana Caramori <musardo@mail.com>
2020-09-07 19:50:07 +0100

    typed js set

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ particlesJS('particles-js',
         }
       },
       "opacity": {
-        "value": 0.7,
+        "value": 0.8,
         "random": false,
         "anim": {
           "enable": false,
@@ -64,7 +64,7 @@ particlesJS('particles-js',
         "enable": true,
         "distance": 150,
         "color": "#ffffff",
-        "opacity": 0.4,
+        "opacity": 0.6,
         "width": 1
       },
       "move": {
